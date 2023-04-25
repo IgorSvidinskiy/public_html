1f86e23a3b069d572b88a6e7ad820f75965c98bd
IgorSvidinskiy <i.s.e18082000@gmail.com>
2023-04-25 10:49:20 +0600

    11

--- register.php
+++ register.php
@@ -11,61 +11,61 @@
   </head>
   <body>
   <?php
-// данные для подключения к базе данных
-$host = 'localhost'; // хост базы данных
-$username = 'root'; // имя пользователя базы данных
-$password = ''; // пароль пользователя базы данных
-$dbname = 'CinemaMonster'; // имя базы данных
+    // данные для подключения к базе данных
+    $host = 'localhost'; // хост базы данных
+    $username = 'root'; // имя пользователя базы данных
+    $password = ''; // пароль пользователя базы данных
+    $dbname = 'CinemaMonster'; // имя базы данных
 
-// подключение к базе данных
-$conn = mysqli_connect($host, $username, $password, $dbname);
+    // подключение к базе данных
+    $conn = mysqli_connect($host, $username, $password, $dbname);
 
-// проверка соединения на ошибки
-if (!$conn) {
-    die('Ошибка подключения: ' . mysqli_connect_error());
-}
-
-// обработка отправки формы
-if (isset($_POST['submit'])) {
-    // получение данных из формы регистрации
-    $username = $_POST['username'];
-    $email = $_POST['email'];
-    $password = password_hash($_POST['password'], PASSWORD_DEFAULT); // хеширование пароля
-
-    // выполнение запроса на добавление нового пользователя в базу данных
-    $sql = "INSERT INTO users (username, email, password) VALUES (NOT NULL, '$username', '$email', '$password')";
-
-    if (mysqli_query($conn, $sql)) 
-    {
-        echo "Новый пользователь успешно добавлен в базу данных";
-        // перенаправление на страницу авторизации
-        header("Location: auth.php");
-        exit();
-    } else {
-        echo "Ошибка: " . $sql . "<br>" . mysqli_error($conn);
+    // проверка соединения на ошибки
+    if (!$conn) {
+        die('Ошибка подключения: ' . mysqli_connect_error());
     }
-}
 
-// закрытие соединения с базой данных
-mysqli_close($conn);
-?>
+    // обработка отправки формы
+    if (isset($_POST['submit'])) {
+        // получение данных из формы регистрации
+        $username = $_POST['username'];
+        $email = $_POST['email'];
+        $password = password_hash($_POST['password'], PASSWORD_DEFAULT); // хеширование пароля
 
-<!-- HTML-код формы регистрации -->
-<form method="post" action="">
-    <label for="username">Имя пользователя:</label>
-    <input type="text" name="username" required><br>
+        // выполнение запроса на добавление нового пользователя в базу данных
+        $sql = "INSERT INTO users (id, username, email, password) VALUES (NOT NULL, '$username', '$email', '$password')";
 
-    <label for="email">E-mail:</label>
-    <input type="email" name="email" required><br>
+        if (mysqli_query($conn, $sql)) 
+        {
+            echo "Новый пользователь успешно добавлен в базу данных";
+            // перенаправление на страницу авторизации
+            header("Location: auth.php");
+            exit();
+        } else {
+            echo "Ошибка: " . $sql . "<br>" . mysqli_error($conn);
+        }
+    }
 
-    <label for="password">Пароль:</label>
-    <input type="password" name="password" required><br>
+    // закрытие соединения с базой данных
+    mysqli_close($conn);
+  ?>
 
-    <label for="confirm_password">Подтверждение пароля:</label>
-    <input type="password" name="confirm_password" required><br>
+  <!-- HTML-код формы регистрации -->
+  <form method="post" action="">
+      <label for="username">Имя пользователя:</label>
+      <input type="text" name="username" required><br>
 
-    <input type="submit" name="submit" value="Зарегистрироваться">
-</form>
+      <label for="email">E-mail:</label>
+      <input type="email" name="email" required><br>
+
+      <label for="password">Пароль:</label>
+      <input type="password" name="password" required><br>
+
+      <label for="password">Подтвердите пароль</label>
+      <input type="password" name="confirm_password" required><br>
+
+      <input type="submit" name="submit" value="Зарегистрироваться">
+  </form>
 
 
   <footer>
